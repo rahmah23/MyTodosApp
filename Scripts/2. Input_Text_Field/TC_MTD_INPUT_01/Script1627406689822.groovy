fdaa9cb54ded9498bb9087d6da617f9354ae8772
Fadhilatur-Rahmah <fadhilatur.rahmah23@gmail.com>
2021-07-28 01:00:47 +0700

    Update Test Case

--- Scripts/2. Input_Text_Field/TC_MTD_INPUT_01/Script1627406689822.groovy
+++ Scripts/2. Input_Text_Field/TC_MTD_INPUT_01/Script1627406689822.groovy
@@ -16,4 +16,41 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.testobject.ConditionType as ConditionType
+
+WebUI.openBrowser(GlobalVariable.URL)
+
+for (def rowNum = 1; rowNum <= findTestData('NewTodoList').getRowNumbers(); rowNum++) {
+	//Create new to-do
+	String newText = findTestData('NewTodoList').getValue(1, rowNum)
+
+	WebUI.setText(findTestObject('My-todos_OR/Page_vue-todos/input_My to-dos_form-control'), newText)
+
+	WebUI.sendKeys(findTestObject('My-todos_OR/Page_vue-todos/input_My to-dos_form-control'), Keys.chord(Keys.ENTER))
+
+	//Verify New Text
+	WebUI.verifyElementText(findTestObject('My-todos_OR/Page_vue-todos/li_activity', [('activity') : newText]), newText)
+
+	//Verify Edit Button
+	String xpathEdit = ('(.//*[normalize-space(text()) and normalize-space(.)="' + newText) + '"])[1]/following::button[1]'
+
+	TestObject editButton = new TestObject('editButton')
+
+	editButton.addProperty('xpath', ConditionType.EQUALS, xpathEdit)
+
+	WebUI.verifyElementPresent(editButton, 0)
+
+	//Verify Delete Button
+	String xpathDelete = ('(.//*[normalize-space(text()) and normalize-space(.)="' + newText) + '"])[1]/following::button[2]'
+
+	TestObject deleteButton = new TestObject('deleteButton')
+
+	deleteButton.addProperty('xpath', ConditionType.EQUALS, xpathDelete)
+
+	WebUI.verifyElementPresent(deleteButton, 0)
+}
+
+WebUI.delay(3)
+
+WebUI.closeBrowser()
 
