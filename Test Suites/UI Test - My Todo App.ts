1cd19c39b529681d722a90e4143fd5c954f591b2
Fadhilatur-Rahmah <fadhilatur.rahmah23@gmail.com>
2021-07-28 01:14:10 +0700

    Add Test Suites

---
+++ Test Suites/UI Test - My Todo App.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UI Test - My Todo App</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>df031eed-8a9c-4d72-b960-c755c0cd27c6</testSuiteGuid>
+   <testCaseLink>
+      <guid>8171759b-b392-4176-9506-013129fe32de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1. App_Initial_Component/TC_MTD_1STCOMP_01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>56cf430e-8cfa-42f0-9e13-45fd81cf855e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2. Input_Text_Field/TC_MTD_INPUT_01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>88435405-60db-4c6e-bc9e-83e8d06dc734</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2. Input_Text_Field/TC_MTD_INPUT_02</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c726f34a-0259-47c7-b80b-95db0cc79ad8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3. Edit Button/TC_MTD_EDIT_01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>08203f58-180a-4a36-9acc-b09d747c6a2e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/4. Delete Button/TC_MTD_DELETE_01</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>af011949-4d66-4b12-8b85-ffd5c945e228</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/5. Complete List/TS_MTD_005</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
